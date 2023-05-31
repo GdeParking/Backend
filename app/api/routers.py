3029c535f6306b4c0520de324ad7631e566371bb
Кривошеев Никита <sharkbreeder@yandex.ru>
2023-05-31 17:18:13 +0300

    prefix

--- app/api/routers.py
+++ app/api/routers.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter
 from app.api.endpoints import camera_router
 
 
-main_router = APIRouter()
+main_router = APIRouter(prefix='/api')
 main_router.include_router(
     camera_router,
     prefix='/camera',
