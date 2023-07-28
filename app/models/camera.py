bdf4cedfcf0157ffa04d62c0cbb7b9cec3e4d188
EvgenSlam <evgenyb86@gmail.com>
2023-07-28 16:05:33 +0300

    modify camera model

--- app/models/camera.py
+++ app/models/camera.py
@@ -1,13 +1,18 @@
 from datetime import datetime
 
-from sqlalchemy import Column, DateTime, Integer, String
+from sqlalchemy import Column, DateTime, Integer, String, Boolean, TIMESTAMP
 
 from app.core.db import Base
 
 
 class Camera(Base):
-    address = Column(String(255), nullable=False)
+    cam_url = Column(String(255), nullable=True)
+    timezone = Column(String(10), nullable=True)
+    address = Column(String(255), nullable=True)
+    update_period = Column(Integer, default=None)
+    consent = Column(Boolean)
     parking_places = Column(Integer, default=0)
-    timezone = Column(String(10))
-    update_period = Column(Integer)
-    last_connection = Column(DateTime, default=datetime.now)
+    last_connection = Column(DateTime, default=datetime.utcnow)
+    registered_at = Column(TIMESTAMP, default=datetime.utcnow)
+    updated_at = Column(TIMESTAMP, default=datetime.utcnow)
+
