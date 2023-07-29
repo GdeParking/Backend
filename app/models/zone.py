8c1536b94c8e602bf4c5690969f8a9d251412053
EvgenSlam <evgenyb86@gmail.com>
2023-07-29 11:26:48 +0300

    modify zone model and make migration

--- app/models/zone.py
+++ app/models/zone.py
@@ -1,12 +1,16 @@
-from sqlalchemy import Boolean, Column, Float, ForeignKey, Integer
+from datetime import datetime
+
+from sqlalchemy import Boolean, Column, Float, ForeignKey, Integer, TIMESTAMP
 
 from app.core.db import Base
 
 
 class Zone(Base):
-    camera_id = Column(Integer, ForeignKey('camera.id'))
-    internal_id = Column(Integer, nullable=False)
-    status = Column(Boolean, default=False)
+    camera_id = Column(Integer, ForeignKey('camera.id')) # extract from Camera table
+    internal_id = Column(Integer, nullable=False) # extract from file
+    status = Column(Boolean, default=False) # False at first, then update from CV
     # FIXME! вот тут добавили поля, удалить при поломке
-    long = Column(Float, default=0)
-    lat = Column(Float, default=0)
+    long = Column(Float, default=0) # extract from file
+    lat = Column(Float, default=0) # extract from file
+    registered_at = Column(TIMESTAMP, default=datetime.utcnow)
+    updated_at = Column(TIMESTAMP, default=datetime.utcnow)
