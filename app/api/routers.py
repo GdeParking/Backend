28fd3557d2dbac1abfd77b45205b668e1ea514f2
EvgenSlam <evgenyb86@gmail.com>
2023-08-17 15:54:29 +0300

    add zone_router to api_router

--- app/api/routers.py
+++ app/api/routers.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.endpoints import camera_router
+from app.api.endpoints import camera_router, zone_router
 
 
 api_router = APIRouter(prefix='/api')
@@ -8,4 +8,10 @@ api_router.include_router(
     camera_router,
     prefix='/camera',
     tags=['CameraInput']
+)
+
+api_router.include_router(
+    zone_router,
+    prefix='/zone',
+    tags=['ZoneCRUD']
 )
